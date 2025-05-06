54b7eecc929dd1fb1d66e2c106f905acc21f95e0
Scott <liysy020@gmail.com>
2025-05-06 16:35:05 +1000

    Replaced SQLite to PostgreSQL

--- Automation/ActiveDefense/settings.py
+++ Automation/ActiveDefense/settings.py
@@ -77,16 +77,25 @@ WSGI_APPLICATION = 'ActiveDefense.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     'OPTIONS': {
+#         'connect_timeout': 10,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'OPTIONS': {
-        'connect_timeout': 10,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'activedefense',
+        'USER': 'activedefenseuser',
+        'PASSWORD': 'activedefensepassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
