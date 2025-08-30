09807a753dff6b880af893e2b53b790e70bd04c7
Scott <liysy020@gmail.com>
2025-08-30 10:38:17 +1000

    Update run.sh

--- Automation/run.sh
+++ Automation/run.sh
@@ -3,6 +3,6 @@
 # Define the absolute path to your Automation folder
 AUTOMATION_DIR="/Automation"
 source "$AUTOMATION_DIR/bin/activate"
-"$AUTOMATION_DIR/bin/gunicorn" ActiveDefense.wsgi:application --bind 127.0.0.1:8000
+"$AUTOMATION_DIR/bin/gunicorn" ActiveDefense.wsgi:application --bind 127.0.0.1:8000 --timeout 600
 
 exit 0
